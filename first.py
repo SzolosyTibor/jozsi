905b4caddfcd406bf167ecc14c5967fdb8c790ae
SzolosyTibor <91724564+SzolosyTibor@users.noreply.github.com>
2023-11-22 16:08:51 +0100

    first.py

--- first.py
+++ first.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.write('Hello, *World!* :sunglasses:')
+st.title("Hello Streamlit!")
+st.header("Fejléc")
+st.subheader("Alsófejléc")
+st.markdown("### Ez valami fos!?")
+st.success("Success")
+st.info("Information")
+st.warning("Warning")
+st.error("Error")
