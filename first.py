c8dbc4ae730b767ccd4d32356d9f6be047b833d0
SzolosyTibor <91724564+SzolosyTibor@users.noreply.github.com>
2023-11-29 15:32:46 +0100

    first.py

--- first.py
+++ first.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def display_power():
     power = 2 ** st.session_state.myslider
-    st.write("A(z)", st.session_state.myslider, "hatványa:", power)
+    st.write("A 2", st.session_state.myslider, "hatványa:", power)
 
 st.slider(
     "Hatvány",
